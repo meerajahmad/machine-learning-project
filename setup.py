7e22a5cb6255e032b6760878bca86d070e01b6c8
Meeraj Ahmad <meeraj.ahmad06@gmail.com>
2025-02-10 21:07:34 +0530

    pyproject.toml commit

--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
+'''
+The setup.py file is an essential part of packaging and
+distributing Python projects. It is used by setuptools
+(or distributing in older python version) to define the configuration
+of your project, such as its metadata. dependencies, and more
+'''
+
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
+def get_requirements()->List[str]:
     '''
     this function will return the list of requirements
     '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
-    return requirements
+    requirements_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement = line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirements_lst.append(requirement)
+    except FileNotFoundError:
+        print('requirements.txt file not found')
+        
+    return requirements_lst
 
 setup(
-name='mlproject',
-version='0.0.1',
-author='Meeraj',
-author_email='meeraj.ahmad06@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+    name= 'mlproject',
+    version= '0.0.1',
+    author= 'Meeraj',
+    author_email= 'meeraj.ahmad06@gmail.com',
+    packages= find_packages(),
+    install_requires= get_requirements()
 )
